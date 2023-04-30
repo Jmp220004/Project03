8fbb7b38f309b06b4e96c69034595fb59337ca0d
Jmpayte <jmp220004@utdallas.edu>
2023-04-29 23:59:00 -0500

    Added User Accessibility

---
+++ Assets/_DeveloperTool/Scripts/ScreenFlashController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenFlashController : MonoBehaviour
+{
+    [Header ("Screen Flash Canvas")]
+    [SerializeField] ScreenFlash _screenFlash = null;
+    [Header ("Screen Flash Color")]
+    [SerializeField] Color _flashColor = Color.red;
+    [Header("Screen Flash Seconds")]
+    [SerializeField] float _flashSeconds = .25f;
+    [Header("Opacity between 0-1")]
+    [SerializeField] float _opacity = .5f;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _screenFlash.StartFlash(_flashSeconds, _opacity, _flashColor);
+        }
+    }
+}
